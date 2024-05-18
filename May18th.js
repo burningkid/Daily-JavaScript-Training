8170e0ddbbaedbde760e09d581e854af7735be6e
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-05-18 07:44:23 +0200

    Update May18th.js

--- May18th.js
+++ May18th.js
@@ -3,3 +3,14 @@
 function makeUpperCase(str) {
   return str.toString().toUpperCase();
 }
+
+// Create a function that takes an integer as an
+// argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+  let num = Math.abs(number);
+  if (num % 2 === 1) {
+    return "Odd";
+  }
+  return "Even";
+}
