5992e688057bad3388ce76b323717862b95bf907
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-05-24 09:03:25 +0200

    Update May24th.js

--- May24th.js
+++ May24th.js
@@ -9,3 +9,26 @@ const stringToNumber = function (str) {
 var stringToNumber = function (str) {
   return +str;
 };
+
+//Given the triangle of consecutive odd numbers:
+
+//             1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1)
+
+function rowSumOddNumbers(n) {
+  return n ** 3;
+}
+
+// function rowSumOddNumbers(n) {
+//   let sum = 0;
+//   let firstItem = (n ^ 2) - n + 1;
+//   for (let i = 0; i < n; i = i + 2) {
+//     sum = firstItem + i;
+//   }
+//   return sum;
+// }
