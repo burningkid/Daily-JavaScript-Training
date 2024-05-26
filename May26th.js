22d0b3eb6e5d39327501f8e9b9bfeace1e575c40
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-05-26 09:46:35 +0200

    Create May26th.js

---
+++ May26th.js
@@ -0,0 +1,25 @@
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+var min = function (list) {
+  let min = list[0];
+
+  for (let i = 1; i < list.length; i++) {
+    if (min > list[i]) {
+      min = list[i];
+    }
+  }
+  console.log(min);
+  return min;
+};
+
+var max = function (list) {
+  let max = list[0];
+
+  for (let i = 1; i < list.length; i++) {
+    if (max < list[i]) {
+      max = list[i];
+    }
+  }
+  console.log(max);
+  return max;
+};
