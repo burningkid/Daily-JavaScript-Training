05e163e79e2317389ebc26c06d718089a426b31b
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-06-02 07:58:52 +0200

    Create Jun2nd.js

---
+++ Jun2nd.js
@@ -0,0 +1,13 @@
+// In this simple assignment you are
+// given a number and have to make
+// it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+  return Math.abs(num) * -1;
+}
+
+// other way
+
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
