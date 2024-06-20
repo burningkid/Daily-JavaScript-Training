4e02a0bf4b83869da7c4299a6e55427da11550e6
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-06-20 18:40:09 +0200

    Create June20th.js

---
+++ June20th.js
@@ -0,0 +1,28 @@
+// Implement a function that adds two
+// numbers together and returns their
+// sum in binary. The conversion can be
+// done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+function addBinary(a, b) {
+  let sum = a + b;
+  let num = sum;
+  let arr = [];
+  while (sum > 0) {
+    if (sum / 2 > 0) {
+      arr.unshift(sum % 2);
+      sum = Math.floor(sum / 2);
+      //       console.log(sum);
+      //     console.log(arr.join(""));
+    }
+  }
+  return arr.join("");
+}
+
+addBinary(5, 9);
