03657c549fbaa9fe11fabed1831379d80c1c68fd
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-07-07 08:07:37 +0200

    Update July7th.js

--- July7th.js
+++ July7th.js
@@ -9,3 +9,12 @@
 // Input: 145263 Output: 654321
 
 // Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+  let arr = n
+    .split("")
+    .map(Number)
+    .sort((a, b) => b - a)
+    .join("");
+  return arr;
+}
