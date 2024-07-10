53b9357c14e276265240510d6c339a44534495d1
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-07-10 16:24:47 +0200

    Update July10th.js

--- July10th.js
+++ July10th.js
@@ -20,3 +20,12 @@
 // the function nb_year should return n number of entire years needed to get a population greater or equal to p.
 
 // aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+function nbYear(p0, percent, aug, p) {
+  let population = 0;
+  let n = 0;
+  population = p0 * (p0 * percent) + aug;
+  n = Math.floor(p / population);
+
+  return n;
+}
