c4759d9fd06cf3979a3c02120a51676e49440372
Kiarash Sajadian <148931588+burningkid@users.noreply.github.com>
2024-08-07 17:34:38 +0200

    Create August07th.js

---
+++ August07th.js
@@ -0,0 +1,27 @@
+// Deoxyribonucleic acid, DNA is the primary information
+// storage molecule in biological systems. It is composed
+// of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger
+// molecule in cells. RNA differs slightly from DNA
+// its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+
+function DNAtoRNA(dna) {
+  let arr = dna.split("");
+  let index = arr.indexOf("T");
+  for (let element of arr) {
+    if (element === "T") {
+      arr[index] = "U";
+    }
+  }
+  return arr.join("");
+}
+
+// other way
+
