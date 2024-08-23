871abbc8059f409c6b68cc41e965b478e8434b13
Kiarash Sajadian <kiarashsajadian@gmail.com>
2024-08-23 09:56:03 +0200

    Create August23th.js

---
+++ August23th.js
@@ -0,0 +1,19 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+var number = function (array) {
+  return array.map(function (line, index) {
+    return index + 1 + ": " + line;
+  });
+};
+
+// arrow function
+
+var number = function (array) {
+  return array.map((line, index) => `${index + 1}: ${line}`);
+};
