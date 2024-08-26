f4ae98f0ce130b39152f62e51ee7cf873ee5013e
Kiarash Sajadian <kiarashsajadian@gmail.com>
2024-08-26 10:06:01 +0200

    Update August26th.js

--- August26th.js
+++ August26th.js
@@ -9,3 +9,19 @@
 // must be removed from the other:
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  let result = [];
+
+  // Loop through each number in array 'a'
+  for (let i = 0; i < a.length; i++) {
+    let number = a[i];
+
+    // If the number is NOT in array 'b', add it to the result
+    if (!b.includes(number)) {
+      result.push(number);
+    }
+  }
+
+  return result;
+}
