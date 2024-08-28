7ac4c5f0d99a9d57e38a130f8981d07d02d7beb7
Kiarash Sajadian <kiarashsajadian@gmail.com>
2024-08-28 06:31:49 +0200

    Update August28th-01.js

--- August28th-01.js
+++ August28th-01.js
@@ -3,3 +3,22 @@
 function stringToArray(string) {
   return string.split(" ");
 }
+
+// Other ways
+
+function stringToArray(string) {
+  var result = [];
+  var traverse = function foo(string) {
+    for (var i = 0; i < string.length; i++) {
+      var chr = string.charAt(i);
+      if (chr === " ") {
+        result.push(string.slice(0, i));
+        foo(string.slice(i + 1));
+        return;
+      }
+    }
+    result.push(string);
+  };
+  traverse(string);
+  return result;
+}
