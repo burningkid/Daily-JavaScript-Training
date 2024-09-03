ebbd4fb8439eb85ec4db0a238e6d9ccb063ae71f
Kiarash Sajadian <kiarashsajadian@gmail.com>
2024-09-03 09:12:59 +0200

    Update September03th.js

--- September03th.js
+++ September03th.js
@@ -34,3 +34,20 @@ function duplicateCount(text) {
 
   return count;
 }
+
+// Other way
+
+function duplicateCount(text) {
+  var lower = text.toLowerCase();
+  var count = 0;
+  var used = [];
+
+  lower.split("").forEach(function (letter) {
+    if (!used.includes(letter) && lower.split(letter).length - 1 > 1) {
+      count++;
+      used.push(letter);
+    }
+  });
+
+  return count;
+}
